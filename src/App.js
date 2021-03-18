5d66c536f9513045bdcad46104d900e28cf570a2
sagostinelli <batonenbois@gmail.com>
2021-03-18 09:42:48 -0400

    test6

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> new test4
+          Edit <code>src/App.js</code> new test5
         </p>
         <a
           className="App-link"
